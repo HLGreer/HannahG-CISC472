2237e822f1117f6f1b303a6b42b788ecbf0bc8be
hlgreer <hannahgreer@rogers.com>
2017-02-06 22:52:52 -0500

    Completion of Homework for Feb 7, 2017.

--- HannahG/HannahG.py
+++ HannahG/HannahG.py
@@ -254,108 +254,118 @@ class HannahGTest(ScriptedLoadableModuleTest):
     self.test_HannahG1()
 
   def test_HannahG1(self):
-    print "Hannah"
+    import numpy
 
-    Scale = 30.0
-    numPerEdge = 3
-    Sigma = 2.0 # radius of random error
-    N = 20
+    def createTransformPoints(randErr, N):
+      Scale = 30.0
+      Sigma = randErr  # radius of random error
 
-    fromNormCoordinates = numpy.random.rand(N, 3) # An array of random numbers
-    noise = numpy.random.normal(0.0, Sigma, N*3)
+      fromNormCoordinates = numpy.random.rand(N, 3)  # An array of random numbers
+      noise = numpy.random.normal(0.0, Sigma, N * 3)
 
-    # Homework for Jan 24:
-    referenceToRas = slicer.vtkMRMLLinearTransformNode()
-    slicer.mrmlScene.AddNode(referenceToRas)
-    referenceToRas.SetName('ReferenceToRas')
+      # Homework for Jan 24:
+      referenceToRas = slicer.vtkMRMLLinearTransformNode()
+      slicer.mrmlScene.AddNode(referenceToRas)
+      referenceToRas.SetName('ReferenceToRas')
 
+      referenceFids = slicer.vtkMRMLMarkupsFiducialNode()
+      referenceFids.SetName('ReferenceFiducials')
+      slicer.mrmlScene.AddNode(referenceFids)
+      referenceFids.GetDisplayNode().SetSelectedColor(0, 0, 1)
+      RASFids = slicer.vtkMRMLMarkupsFiducialNode()
+      RASFids.SetName('RASFiducials')
+      slicer.mrmlScene.AddNode(RASFids)
+      RASFids.GetDisplayNode().SetSelectedColor(1, 1, 0)
 
-    referenceFids = slicer.vtkMRMLMarkupsFiducialNode()
-    referenceFids.SetName('ReferenceFiducials')
-    slicer.mrmlScene.AddNode(referenceFids)
-    referenceFids.GetDisplayNode().SetSelectedColor(0, 0, 1)
-    RASFids = slicer.vtkMRMLMarkupsFiducialNode()
-    RASFids.SetName('RASFiducials')
-    slicer.mrmlScene.AddNode(RASFids)
-    RASFids.GetDisplayNode().SetSelectedColor(1, 1, 0)
+      alphaPoints = vtk.vtkPoints()
+      betaPoints = vtk.vtkPoints()
+      for i in range(N):
+        x = (fromNormCoordinates[i, 0] - 0.5) * Scale
+        y = (fromNormCoordinates[i, 1] - 0.5) * Scale
+        z = (fromNormCoordinates[i, 2] - 0.5) * Scale
+        numFids = referenceFids.AddFiducial(x, y, z)
+        numPoints = alphaPoints.InsertNextPoint(x, y, z)
+        xx = x + noise[i * 3]
+        yy = y + noise[i * 3 + 1]
+        zz = z + noise[i * 3 + 2]
+        numFids = RASFids.AddFiducial(xx, yy, zz)
+        numPoints = betaPoints.InsertNextPoint(xx, yy, zz)
 
-    alphaPoints = vtk.vtkPoints()
-    betaPoints = vtk.vtkPoints()
-
-    for i in range(N):
-      x = (fromNormCoordinates[i, 0] - 0.5) * Scale
-      y = (fromNormCoordinates[i, 1] - 0.5) * Scale
-      z = (fromNormCoordinates[i, 2] - 0.5) * Scale
-      numFids = referenceFids.AddFiducial(x, y, z)
-      numPoints = alphaPoints.InsertNextPoint(x, y, z)
-      xx = x+noise[i*3]
-      yy = y+noise[i*3+1]
-      zz = z+noise[i*3+2]
-      numFids = RASFids.AddFiducial(xx, yy, zz)
-      numPoints = betaPoints.InsertNextPoint(xx, yy, zz)
+      return [alphaPoints, betaPoints, referenceToRas]
 
     # Create landmark transform object that computes registration
+    def computeRegistration(referenceToRas, alphaPoints, betaPoints):
+      landmarkTransform = vtk.vtkLandmarkTransform()
+      landmarkTransform.SetSourceLandmarks(alphaPoints)
+      landmarkTransform.SetTargetLandmarks(betaPoints)
+      landmarkTransform.SetModeToRigidBody()
+      landmarkTransform.Update()
 
-    landmarkTransform = vtk.vtkLandmarkTransform()
-    landmarkTransform.SetSourceLandmarks( alphaPoints )
-    landmarkTransform.SetTargetLandmarks( betaPoints )
-    landmarkTransform.SetModeToRigidBody()
-    landmarkTransform.Update()
+      alphaToBetaMatrix = vtk.vtkMatrix4x4()
+      landmarkTransform.GetMatrix(alphaToBetaMatrix)
 
-    alphaToBetaMatrix = vtk.vtkMatrix4x4()
-    landmarkTransform.GetMatrix( alphaToBetaMatrix )
+      det = alphaToBetaMatrix.Determinant()
+      if det < 1e-8:
+        print 'Unstable registration. Check input for collinear points.'
 
-    det = alphaToBetaMatrix.Determinant()
-    if det < 1e-8:
-      print 'Unstable registration. Check input for collinear points.'
-
-    referenceToRas.SetMatrixTransformToParent(alphaToBetaMatrix)
+      referenceToRas.SetMatrixTransformToParent(alphaToBetaMatrix)
+      return alphaToBetaMatrix
 
     # Compute average point distance after registration
 
-    average = 0.0
-    numbersSoFar = 0
+    def avgDistAfterReg(N, alphaPoints, betaPoints, alphaToBetaMatrix):
+      average = 0.0
+      numbersSoFar = 0
+      for i in range(N):
+        numbersSoFar = numbersSoFar + 1
+        a = alphaPoints.GetPoint(i)
+        pointA_Alpha = numpy.array(a)
+        pointA_Alpha = numpy.append(pointA_Alpha, 1)
+        pointA_Beta = alphaToBetaMatrix.MultiplyFloatPoint(pointA_Alpha)
+        b = betaPoints.GetPoint(i)
+        pointB_Beta = numpy.array(b)
+        pointB_Beta = numpy.append(pointB_Beta, 1)
+        distance = numpy.linalg.norm(pointA_Beta - pointB_Beta)
+        average = average + (distance - average) / numbersSoFar
 
-    for i in range(N):
-      numbersSoFar = numbersSoFar + 1
-      a = alphaPoints.GetPoint(i)
-      pointA_Alpha = numpy.array(a)
-      pointA_Alpha = numpy.append(pointA_Alpha, 1)
-      pointA_Beta = alphaToBetaMatrix.MultiplyFloatPoint(pointA_Alpha)
-      b = betaPoints.GetPoint(i)
-      pointB_Beta = numpy.array(b)
-      pointB_Beta = numpy.append(pointB_Beta, 1)
-      distance = numpy.linalg.norm(pointA_Beta - pointB_Beta)
-      average = average + (distance - average) / numbersSoFar
+      print "Average distance after registration: " + str(average)
+      return average
 
-    print "Average distance after registration: " + str(average)
 
-    # from Jan 26
-    createModelsLogic = slicer.modules.createmodels.logic()
+      # For Feb 2nd:
 
-    refModelNode = createModelsLogic.CreateCoordinate(20, 2)
-    refModelNode.SetName('ReferenceModel')
-    refModelNode.GetDisplayNode().SetColor(1,0,0)
+      # Compute Target Registration Error:
+      # Target registration error (TRE), which is the distance, after registration,
+      # between a pair of corresponding points which are not used in registration.
+      # (From http://www.cse.yorku.ca/~burton/publications/A%20theoretical%20comparison%20of%20different%20target%20registration%20error%20estimators.pdf)
 
-    RASModelNode = createModelsLogic.CreateCoordinate(20, 2)
-    RASModelNode.SetName('RASModel')
-    RASModelNode.GetDisplayNode().SetColor(0, 0, 1)
+      # Transform the point (0,0,0) with the transform and return the distance between the original and the transformed
+      # point as the TRE
 
-    RefToRasTransform = referenceToRas.GetTransformToParent()
+    def computeTRE(alphaToBetaMatrix):
+      targetPoint = numpy.array((0, 0, 0, 1))
+      transformedTarget = alphaToBetaMatrix.MultiplyFloatPoint(targetPoint)
+      TRE = numpy.linalg.norm(transformedTarget - targetPoint)
+      print "The Target Registration Error is: "
+      print TRE
+      return TRE
 
-    # Compute Target Registration Error:
-    # Target registration error (TRE), which is the distance, after registration,
-    # between a pair of corresponding points which are not used in registration.
-    # (From http://www.cse.yorku.ca/~burton/publications/A%20theoretical%20comparison%20of%20different%20target%20registration%20error%20estimators.pdf)
+    # homework for Feb 7, 2017
+    def compareTRE_FRE():
+      for i in range(10, 40, 5):
+        [alphaPoints, betaPoints, referenceToRas] = createTransformPoints(2.0, i)
+        alphaToBetaMatrix = computeRegistration(referenceToRas, alphaPoints, betaPoints)
+        average = avgDistAfterReg(i, alphaPoints, betaPoints, alphaToBetaMatrix)
+        TRE = computeTRE(alphaToBetaMatrix)
 
-    # Transform the point (0,0,0) with the transform and return the distance between the original and the transformed
-    # point as the TRE
 
-    targetPoint = numpy.array((0,0,0,1))
-    transformedTarget = alphaToBetaMatrix.MultiplyFloatPoint(targetPoint)
-    TRE = numpy.linalg.norm(transformedTarget - targetPoint)
-    print "The Target Registration Error is: "
-    print TRE
+
+
+
+
+
+
+
 
 
 
